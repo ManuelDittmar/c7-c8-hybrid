28424fc99d75d7f3a072df6a600b71ee38589410
ManuelDittmar <manuel.dittmar@camunda.com>
2023-09-28 16:20:12 +0200

    Auto-Deploy

--- src/main/java/org/camunda/consulting/example/Application.java
+++ src/main/java/org/camunda/consulting/example/Application.java
@@ -16,10 +16,12 @@
  */
 package org.camunda.consulting.example;
 
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Deployment(resources = "classpath*:/bpmn/**/*-c8.bpmn")
 public class Application {
 
   public static void main(String... args) {
